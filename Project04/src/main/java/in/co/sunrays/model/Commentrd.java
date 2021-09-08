4e6de3bee9a4e120107de517301a54551982a56e
mayan0789 <master.raj25@gmail.com>
2021-09-08 16:03:11 +0530

    project test

--- Project04/src/main/java/in/co/sunrays/model/Commentrd.java
+++ Project04/src/main/java/in/co/sunrays/model/Commentrd.java
@@ -4,6 +4,17 @@ public class Commentrd {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello  javaaa");
+	
+	System.out.println("Never");
+	
+	add();
+	
 	}
+
+	private static void add() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
